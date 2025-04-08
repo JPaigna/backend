66f962185fe6e7efe91dcb676e28cd6b5a40c7e1
JPaigna <paignajeason@gmail.com>
2025-04-08 14:52:17 +0800

    late nami

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,11 +44,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',  # Add comma here
+    'django.contrib.messages.middleware.MessageMiddleware', 
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 
@@ -58,6 +59,20 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+]
 
 
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from myapp.views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r'myapp', TaskViewSet)
+router.register('myapp', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
