2e7bcd2fa462a60808b63148a8fee5dc5a806765
JPaigna <126860913+JPaigna@users.noreply.github.com>
2025-04-15 10:32:08 +0800

    updated file

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from myapp.views import TaskViewSet, SecureHelloView
 
 # Set up the DRF router
@@ -28,7 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),  
     path('api/todos/', include('myapp.urls')),  
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    # Removed legacy token auth endpoint to avoid confusion
+    # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('access-denied/', SecureHelloView.as_view(), name='access-denied'),
 ]
 
